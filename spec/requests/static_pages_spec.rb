1f5cd4bb36c10196ff50b65c62f7179d0e0cd930
Jarmo <nurse.jason@gmail.com>
2013-12-27 11:49:16 -0500

    Finish user edit, update, index, and destroy actions

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,8 +4,6 @@ describe "StaticPages" do
 
   	subject { page }
 
-
-
   	shared_examples_for "all static pages" do
 		it { should have_selector('h1', text: heading) }
 		it { should have_title(full_title(page_title)) }
